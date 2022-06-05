fa02fb5cf92cbc473cf3bdf7ef9c5b17088d50bb
FederalTN <espada_2255@hotmail.com>
2022-06-05 16:30:32 -0700

    Comentado listo, arreglos menores.

--- FuncionesAPD.h
+++ FuncionesAPD.h
@@ -3,50 +3,76 @@
 
 using namespace std;
 
-struct nodo{
-  string info;
-  struct nodo *nexo;
+struct nodo{           // Lista enlazada que posteriormente utilizaremos para guardar los estados finales
+  string info;        // estado final almacenada en la info del nodo
+  struct nodo *nexo;  // puntero al siguiente nodo
 };
 typedef struct nodo *Lista;
 
-void inputs(string &estado, Lista &eFinales,string &einicial){
+void ingresoTransiciones(map<tuple<string,char,char>,tuple<string, string>> &M);
+void quitaEspacio(string &palabra);
+string aMinuscula(string str, int n);
+bool esAlfanumerico(char c);
+bool validaPalabra(string str, int &i, int n, string &output);
+bool validaSimbolo(string str, int &i, int n, char &output);
+bool validaChar(string str,char obj, int &i, int n);
+bool conseguirDatos(string qi, char a, char R, string &qj, string &AR, map<tuple<string,char,char>,tuple<string, string>> &M);
+void ManejoError(int opt);
+void mensajeAyuda();
+void imprimirTransiciones(map<tuple<string,char,char>,tuple<string, string>> &M);
+void imprimirDI(string p, string s, string q, int i);
+void insertarInfo(Lista &eFinales, string estadoF);
+bool esAlfanumerico(string s);
+void inputs(bool &aceptaF, Lista &eFinales,string &einicial);
+char avanzarStack(string &stack, string AR);
+char avanzaPalabra(string palabra, int &i);
 
-    cout<<"Ingrese el estado inicial: ";
-    getline(cin,einicial);
-    quitaEspacio(einicial);
-    cout<<endl;
-	
-    cout<<"Ingrese (1) si el APD acepta por stack vacio y (2) si acepta por estado final: ";
-    getline(cin, estado);
-    quitaEspacio(estado);
-    cout<<endl;
-    string temp;
-    if(estado=="2"){
-        while(true){
-          cout << "Ingrese un estado final: " << endl;
-          getline(cin, temp);
-          quitaEspacio(temp);
-          if(temp.length() == 0) {
-            ManejoError(1);
-            continue;
-          }
-          if(temp == "siguiente") break;
-          if(esAlfanumerico(temp)){
-            insertarInfo(eFinales, temp);
-            cout << "ingreso Exitoso!" << endl;
-          } else {
-            ManejoError(4);
-          }
-        }
-        cout<<endl;
+// Funcion inputs: Funcion para el ingreso de las variables solicitadas
+// aceptaF: si acepta por estados finales o no
+// eFinales: lista enlazada con los estados finales.
+// einicial: estado inicial
+void inputs(bool &aceptaF, Lista &eFinales,string &einicial){
+    string estado, temp;
+    cout<<"====================================\n";
+    while(true){                                            // while para el ingreso de la palabra inicial
+      cout<<"Ingrese el estado inicial: ";
+      getline(cin,einicial);
+      quitaEspacio(einicial);
+      if(einicial == "")                  ManejoError(1);
+      else if (!esAlfanumerico(einicial)) ManejoError(4);
+      else break;
     }
-
-    cout<< endl<<" ===================================="<<endl<<endl
+    cout<<"====================================\n";
+    while(true){                                            // while para el ingreso de cual metodo utiliza el APD para aceptar palabras.
+      cout<<"Ingrese (1) si el APD acepta por stack vacio y (2) si acepta por estado final: ";
+      getline(cin, estado);
+      quitaEspacio(estado);
+      cout<<endl;
+      if (estado == "") ManejoError(1);
+      else if ((estado != "1") && (estado != "2")) ManejoError(0);
+      else break;
+    }
+    aceptaF = (estado == "2");
+    cout<<"====================================\n";
+    while(aceptaF){                                         // while para el ingreso de estados finales, mientras el APD utilice estados finales.
+      cout << "Ingrese un estado final: ";
+      getline(cin, temp);
+      quitaEspacio(temp);
+      if(temp.length() == 0) {
+        ManejoError(1);
+        continue;
+      }
+      if(temp == "siguiente") break;
+      if(esAlfanumerico(temp)){
+        insertarInfo(eFinales, temp);
+        cout << "ingreso Exitoso!" << endl;
+      } else {
+        ManejoError(4);
+      }
+    }
+    cout<<"====================================\n"
         << "Estado inicial: "<<einicial<<endl;
-    if(estado=="1"){
-        cout<<"El APD acepta por stack vacio por lo que no tiene estado final."<<endl;
-    }
-    else{
+    if(aceptaF){                                            // imprime los estados finales, Si el APD acepta por estado final.
         Lista p = eFinales;
         cout<<"Los estados finales del APD son: " << endl;
         while(p != NULL){
@@ -56,79 +82,66 @@ void inputs(string &estado, Lista &eFinales,string &einicial){
         cout << endl;
     }
 }
-bool validarVacio(string palabra){ //Valida si la palabra es vacía
-    int n=palabra.length();
-    if(n==0){
-        return true;
-    }
-    int cont = 0;
-    for(int i = 0; i<n; i++){
-        if(palabra[i]!=' '){
-            cont++;
-        }
-    }
-    if(cont>0){
-        return false;
-    }
-    return true;
-}
+// Funciòn ingresoTransiciones: ingresa transiciones a la estructura utilizada
+// M: map utilizado para almacenar transiciones
 void ingresoTransiciones(map<tuple<string,char,char>,tuple<string, string>> &M){
-  string input="",qi,qj,AR,t1,t2; // qi es estado actual, qj es estado destino, AR simbolos que se adieren al stack 
-  char a,R; //a simbolo que se lee de la palabra, R simbolo que se lee del stack. EJ del todo: (qi,a,R)=(qj,AR)
-  int i,n;
-  bool elim;
-    cout << "====================================\n"
-         << "Las transiciones se ingresan de la forma: (qi,a,R)=(qj,B) \n"
-         << "El simbolo \'@\' representa la palabra vacia.\n"
-         << "Para dejar de ingresar transiciones, escriba \'siguiente\' \n"
-         << "Si necesita mas ayuda, ingrese \'ayuda\' \n";
-  while(true){
+  string input="",qi,qj,AR,t1,t2; // qi es estado actual, qj es estado destino, AR simbolos que se adieren al stack.
+  char a,R;                       // a simbolo que se lee de la palabra, R simbolo que se lee del stack. EJ del todo: (qi,a,R)=(qj,AR).
+  int i,n;                        // i es un indice utilizado para leer el ingreso y n es el tamaño del ingreso.
+  bool elim;                      // booleano que explica si el ingreso hace referencia a borrar una transiciòn.
+  cout << "====================================\n"
+      << "Las transiciones se ingresan de la forma: (qi,a,R)=(qj,B) \n"
+      << "El simbolo \'@\' representa la palabra vacia.\n"
+      << "Para dejar de ingresar transiciones, escriba \'siguiente\' \n"
+      << "Si necesita mas ayuda, ingrese \'ayuda\'\n";
+  while(true){                                   //while para ingresar transiciones
     cout << "Ingresa la transicion: ";
     getline(cin,input);
-    quitaEspacio(input);
+    quitaEspacio(input);                         //Quita espacios del ingreso
 
     i=0;
-    n=input.length();
+    n=input.length();                            //las variables i,n se actualizan con el ingreso actual
     
     if(aMinuscula(input,n)=="siguiente") break;  //se sale del ingreso de transiciones
-    if(aMinuscula(input,n)=="ayuda"){
+    if(aMinuscula(input,n)=="ayuda"){            //Imprime el mensaje de ayuda y se salta las validaciones siguientes para el input.
       mensajeAyuda();
       continue;
     }
-    if(aMinuscula(input,n)=="visualizar"){
+    if(aMinuscula(input,n)=="visualizar"){       //Imprime las transiciones y se salta las validaciones siguientes para el input.
       imprimirTransiciones(M);
       continue;
     }
-    if(n==0){  //se ingresa vacio
+    if(n==0){                                    //se ingresa vacio
       ManejoError(1);
       continue;
     }
 
-    if(input[i]=='-'){
+    if(input[i]=='-'){                           //se considera que el ingreso busca eliminar una transicion
       elim=true;
       i++;
     }
     else elim=false;
 
+    // valida que la entrada sea de la forma (estado inicial, simbolo de la palabra a leer, simbolo al tope del stack) = (estado resultante, palabra a añadir al stack)
     if(!(validaChar(input,'(',i,n)&& validaPalabra(input,i,n,qi)&& validaChar(input,',',i,n)&& validaSimbolo(input,i,n,a))) continue;
     if(!(validaChar(input,',',i,n)&& validaSimbolo(input,i,n,R)&& validaChar(input,')',i,n)&& validaChar(input,'=',i,n))) continue;
     if(!(validaChar(input,'(',i,n)&& validaPalabra(input,i,n,qj)&& validaChar(input,',',i,n)&& validaPalabra(input,i,n,AR)&& validaChar(input,')',i,n))) continue;
-    if(i<n){
+    if(i<n){                                     //El ingreso es una transicion pero le proceden simbolos innecesarios
       ManejoError(3);
       continue;
     }
-    if(a=='@' && R=='@'){
+    if(a=='@' && R=='@'){                        //Se prohibe el ingreso de una transiciòn que ocupe palabra vacia y stack vacio (provoca while infinito). 
         ManejoError(6);
         continue;
     }
-    if(elim){
-      if(conseguirDatos(qi,a,R,t1,t2,M) && qj==t1 && AR==t2){
-          M.erase({qi,a,R});
+    if(elim){                                    //Si se busca eliminar la transiciòn 
+      if(conseguirDatos(qi,a,R,t1,t2,M) && qj==t1 && AR==t2){ //Si existe una transiciòn con esa entrada y la salida corresponde a esta
+          M.erase({qi,a,R});                                  //se borra la transiciòn
           cout<<"Eliminacion exitosa.\n";
         }
         else ManejoError(5);
     }
-    else{
+    else{                                        //se ingresa la transiciòn
       cout<<"Ingreso exitoso. ";
       if(M.count({qi,a,R})==1) cout << "Sobreescribiendo transicion";
       cout << endl;
@@ -136,6 +149,8 @@ void ingresoTransiciones(map<tuple<string,char,char>,tuple<string, string>> &M){
     }
   }
 }
+// funcion quitaEspacio: quita los espacios de la palabra ingresada.
+// palabra: string que se busca eliminar los espacios.
 void quitaEspacio(string &palabra){
     string palabraSinEsp="";
     int n=palabra.length();
@@ -147,6 +162,9 @@ void quitaEspacio(string &palabra){
     }
     palabra=palabraSinEsp;
 }
+// funcion aMinuscula: Devuelve un string que corresponde al string ingresado pero en minuscula.
+// str: string ingresado (se utiliza para intentos de ingresar: ayuda, siguiente y finalizar) en el programa
+// n: tamaño del string ingresado
 string aMinuscula(string str, int n){
   string output="";
   for(int i=0; i<n;i++){
@@ -157,6 +175,8 @@ string aMinuscula(string str, int n){
   }
   return output;
 }
+// funcion esAlfanumerico(char): Devuelve si un caracter corresponde a un valor alfanumerico o no.
+// c: caracter ingresado.
 bool esAlfanumerico(char c){
   int i= (int)(c); //consigue el codigo ASCII del char c
   if(48<=i && i<=57) return true; //es un numero
@@ -165,12 +185,19 @@ bool esAlfanumerico(char c){
   if(64==i) return true; //es @ (se utiliza como palabra vacia)
   return false;
 }
+// funcion esAlfanumerico(str): Devuelve si un string esta compuesto de caracteres alfanumericos o no.
+// s: string ingresado.
 bool esAlfanumerico(string s){
   int n = s.length();
   for(int i=0; i<n; i++)
     if(!esAlfanumerico(s[i])) return false;
   return true;
 }
+// funcion validaPalabra: Valida si una palabra, desde una posicion del string ingresado, existe.
+// str: string ingresado.
+// i: posiciòn del string desde el que se valida (se pasa por referencia para avanzar i)
+// n: tamaño del string ingresado.
+// output: variable pasada por referencia que contendra la palabra.
 bool validaPalabra(string str, int &i, int n, string &output){
   if(i>=n){
     ManejoError(2);
@@ -185,6 +212,11 @@ bool validaPalabra(string str, int &i, int n, string &output){
   output=str.substr(j,i-j);
   return true;
 }
+// funcion validaSimbolo: Valida si un simbolo, en una posicion del string ingresado, existe.
+// str: string ingresado.
+// i: posiciòn del string en la que se valida (se pasa por referencia para avanzar i)
+// n: tamaño del string ingresado.
+// output: variable pasada por referencia que contendra el simbolo.
 bool validaSimbolo(string str, int &i, int n, char &output){
   if(i>=n){
     ManejoError(2);
@@ -198,6 +230,11 @@ bool validaSimbolo(string str, int &i, int n, char &output){
   ManejoError(4);
   return false;
 }
+// funcion validaChar: Valida si un char, en una posicion del string ingresado, es igual al caracter esperado.
+// str: string ingresado.
+// obj: caracter esperado.
+// i: posiciòn del string en la que se valida (se pasa por referencia para avanzar i)
+// n: tamaño del string ingresado.
 bool validaChar(string str,char obj, int &i, int n){
   if(i>=n){
     ManejoError(2);
@@ -210,14 +247,23 @@ bool validaChar(string str,char obj, int &i, int n){
   ManejoError(0);
   return false;
 }
+// funcion conseguirDatos: devuelve si existe la transicion segun los datos ingresados
+// qi: estado actual
+// a: simbolo de la palabra a leer
+// R: simbolo en el tope del stack
+// qj: estado resultante (si existe, esta variable lo contendra)
+// AR: palabra que se añade al stack (si existe, esta variable lo contendra)
+// M: map utilizado para almacenar transiciones.
 bool conseguirDatos(string qi, char a, char R, string &qj, string &AR,map<tuple<string,char,char>,tuple<string, string>> &M){
-  if(M.count({qi,a,R})!=1){
-    return false;
+  if(M.count({qi,a,R})!=1){                   // Si no existe valor en el map para la llave compuesta del estado actual del APD {qi, a, R}
+    return false;                             // Retorna falso, no existe transiciòn.
   }
-  qj=get<0>(M[{qi,a,R}]);
+  qj=get<0>(M[{qi,a,R}]);                     // Le da los valores resultantes de la transiciòn a qj y a AR
   AR=get<1>(M[{qi,a,R}]);
-  return true;
+  return true;                                // Retorna verdadero si existe transiciòn.
 }
+// funcion ManejoError: recibe un valor entero que sirve como codigo para imprimir una razon de error.
+// opt: valor entero ingresado
 void ManejoError(int opt){
   cout << "====================================\nERROR:\t";
   switch (opt){
@@ -246,8 +292,8 @@ void ManejoError(int opt){
     cout<<"Error no definido.\n";
     break;
   }
-  cout << "Si necesita ayuda, ingrese \'ayuda\'\n";
 }
+// mensaje de ayuda
 void mensajeAyuda(){
   cout<<"====================================\n"
       <<"El ingreso de transiciones debe hacerse individualmente y de la forma: (qi,a,R)=(qj,B)\n"
@@ -259,6 +305,8 @@ void mensajeAyuda(){
       <<"Para dar termino al ingreso de transiciones, ingrese \'siguiente\'\n"
       <<"Solo debe ingresar valores alfanumericos.\nCualquier espacio en blanco es ignorado, para ingresar la palabra vacia, use \'@\'.\n";
 }
+// funcion imprimirTransiciones: imprime las transiciones almacenadas en el map.
+// M: map utilizado para almacenar transiciones.
 void imprimirTransiciones(map<tuple<string,char,char>,tuple<string, string>> &M){
   cout<<"====================================\n"
       << "Transiciones del APD:\n";
@@ -273,42 +321,52 @@ void imprimirTransiciones(map<tuple<string,char,char>,tuple<string, string>> &M)
     cout << '(' << qi << ',' << a << ',' << R << ")=(" << qj << ',' << AR << ')' << endl;
   }
 }
+// funcion avanzarStack: elimina el caracter al inicio de la palabra que representa el stack. Luego añade AR al stack y devuelve el caracter a al tope del stack.
+// stack: string que representa el stack.
+// AR: palabra a añadir al stack.
 char avanzarStack(string &stack, string AR){
-    if(stack.length()>1){
-        stack=stack.substr(1,stack.length()-1);
-    }else{
-        stack="";
-    }
-    if(AR!="@"){
-        stack=AR+stack;
-    }
-    if(stack.length()==0){
-        return '@';
-    }else{
-        return stack[0];
-    }
+    if(stack.length()>1)                            // si en el stack hay mas de 1 caracter
+        stack=stack.substr(1,stack.length()-1);     // el stack se actualiza sin contemplar el primer caracter que poseia (simbolo al tope del stack)
+    else stack="";                                  // caso contrario pasa a ser un string vacio
+    
+    if(AR!="@") stack=AR+stack;                     // si al stack no se le va a añadir la palabra vacia, a este se le añade la palabra
+    
+    if(stack.length()==0) return '@';               // si el stack esta vacio, retorna un simbolo que representa el vacio
+    else return stack[0];                           // si no, retorna un caracter que esta al tope del stack.
 }
+// funcion avanzaPalabra: avanza la palabra segun el contador de posiciòn y 
+// retorna el caracter que representa vacio si la posiciòn esta mas alla del largo de la palabra. Caso contrario retorna el caracter en la posicion de la palabra.
+// palabra: string de la palabra que se esta leyendo.
+// i: contador de posiciòn (pasado por referencia).
 char avanzaPalabra(string palabra, int &i){
-    i++;
-    int n=palabra.length();
-    if(i>=n){
+    i++;                                            // Se avanza el contador considerando que se avanza de simbolo
+    int n=palabra.length();                         // n largo de la palabra
+    if(i>=n){                                       // Si i se encuentra mas alla del largo de la palabra, retorna el simbolo que representa el vacio.
         return '@';
-    }else{
+    }else{                                          // Si no, retorna el caracter en la siguiente posicion de la palabra.
         return palabra[i];
     }
 }
+// funcion imprimirDI: imprime una descripciòn instantanea 
+// p: palabra que se lee
+// s: stack 
+// q: estado actual
+// i: posiciòn de la palabra
 void imprimirDI(string p, string s, string q, int i){
-  int n= p.length(), j= n-i;
-  string pal, stack;
+  int n= p.length(), j= n-i;                        // n largo de la palabra, j cantidad de simbolos que quedan en la palabra.
+  string pal, stack;                                // strings que representan como se imprimiran la palabra y el stack.
   
-  if(i >= n) pal = "@";
-  else pal = p.substr(i,j);
+  if(i >= n) pal = "@";                             // Si i se encuentra mas alla del largo de la palabra, utiliza el simbolo que representa el vacio como palabra.
+  else pal = p.substr(i,j);                         // Si no, utiliza los simbolos que quedan por leer como palabra.
 
-  if (s == "") stack = "@";
-  else stack = s;
+  if (s == "") stack = "@";                         // Si el stack esta vacio, utiliza el simbolo que representa el vacio como stack.
+  else stack = s;                                   // Si no, utiliza directamente el stack.
 
-  cout << "(" << q << ", " << pal << ", " << stack << ")";
+  cout << "(" << q << ", " << pal << ", " << stack << ")";  // Se imprime segun lo que se verifico anteriormente.
 }
+// funcion insertarInfo: inserta estados finales en la lista enlazada Lista.
+// eFinales: Lista enlazada ingresada.
+// estadoF: estado final que se va a ingresar a la lista enlazada.
 void insertarInfo(Lista &eFinales, string estadoF){
   Lista q;
   q = new(struct nodo);
--- main.cpp
+++ main.cpp
@@ -6,62 +6,61 @@
 #include "FuncionesAPD.h"
 
 int main(){
-    string einicial, estado, pEntrada;
-    Lista eFinales=NULL;
-    map<tuple<string,char,char>,tuple<string, string>> T;
+    string einicial, pEntrada;                    //Declaramos las variables que representan estado inicial y palabra entrada
+    bool aceptaF;                                 //bool denota de que metodo acepta el APD (FALSE por stack vacio / TRUE por estado final)
+    Lista eFinales=NULL;                          //Lista enlazada de estados finales 
+    map<tuple<string,char,char>,tuple<string, string>> T; // map que almacena transiciones donde la key es la situacion actual y value el resultado
+                                                          // key: {estado actual, simbolo actual de la palabra, simbolo en el tope del stack}
+                                                          // value: {estado resultante, palabra a añadir al stack}
     cout << "====================================\n";
     cout << "        AUTOMATA PUSH DOWN          \n";
 
-    ingresoTransiciones(T);
-    inputs(estado, eFinales, einicial);
-    string qi, qj, AR;      //AR= lo que se le añadira al stack   qi= estado actual    qj= estado al que se ira
+    ingresoTransiciones(T);                       //Funcion de ingreso de transiciones
+    inputs(aceptaF, eFinales, einicial);          //Funcion de ingreso de las variables argumento
+    string qi, qj, AR;                            //AR= lo que se le añadira al stack   qi= estado actual    qj= estado al que se ira
+    // while para ingresar palabras al APD 
     while(true){
-
-      /*Ingreso palabras*/
-
-      cout<<"Ingrese palabra de entrada: ";
-      getline(cin,pEntrada);
-      quitaEspacio(pEntrada);
-      if(pEntrada=="finalizar"){
-        break;
-      }
-      while(validarVacio(pEntrada)){
-        cout<<"Ingrese palabra de entrada valida (No vacia): ";
+      while (true){   // while para ingresar una palabra
+        cout<<"\nIngrese palabra de entrada: ";
         getline(cin,pEntrada);
-      }
-
+        quitaEspacio(pEntrada);                   // Se quitan los espacios de la palabra ingresada.
+        if(pEntrada == "") {                      // Si se ingresa una palabra vacia, se pide otra palabra para ingresar o para finalizar
+          ManejoError(1);
+          continue;
+        }
+        if(pEntrada=="finalizar") exit(EXIT_SUCCESS);    // Si se ingresa finalizar, se finaliza el programa.
+        else break;
+      } // Si el ingreso no tuvo ningun error, se hace un break del while
 
       cout<<"La palabra de entrada es: "<<pEntrada<<endl<<endl;
 
+      char a=pEntrada[0];       //a = simbolo actual, corresponde al primer char de la palabra de entrada.
+      int i = 0;                //Contador de posiciòn en la palabra.
+      string stack = "R";       //String que simboliza el stack.
+      qi=einicial;              //Variable que denota el estado actual, se inicializa como estado inicial.
+      char S = stack[0];        //Char en el tope del stack
+      imprimirDI(pEntrada, stack, qi, i); // Descripciòn instantanea.
 
-      char a=pEntrada[0];       //a = simbolo actual    tope del stack
-      int i = 0;
-      string stack = "R";
-      qi=einicial;
-      char S = stack[0];
-      imprimirDI(pEntrada, stack, qi, i);
-
-      while(conseguirDatos(qi,a,S,qj,AR, T)){
-          qi=qj;
-          a=avanzaPalabra(pEntrada,i);
-          S=avanzarStack(stack, AR);
-          cout << "├" ;
-          imprimirDI(pEntrada, stack, qi, i);
+      while(conseguirDatos(qi,a,S,qj,AR, T)){ //while que recorre la palabra con el APD. Depende de que conseguirDatos tenga datos para devolver (TRUE) o no (FALSE).
+          qi=qj;                              //El estado actual pasa a ser el estado resultante de la transicion(qj)
+          a=avanzaPalabra(pEntrada,i);        //Se avanza la palabra
+          S=avanzarStack(stack, AR);          //Se elimina el simbolo en el tope del stack. Luego añade la palabra indicada por la transicion en el stack.
+          cout << "├" ;                       
+          imprimirDI(pEntrada, stack, qi, i); // Descripciòn instantanea.
       }
       cout<<endl<<endl;
-      Lista p = eFinales;
-      while(p != NULL && (p->info != qi)){
-         p = p->nexo;
+      if(aceptaF){                               //Validaciòn de la palabra segun estado final
+        Lista p = eFinales;                     //p apunta a la lista de estados finales.
+        while(p != NULL && (p->info != qi))    //mientras p no sea NULL y el info de p sea diferente al estado en el que estamos
+          p = p->nexo;                        //se avanza p
+        if(p != NULL)                        //Si p no es NULL. se encontro un estado final igual al estado con el que termina de leer la palabra el APD
+          cout<<"Acepta la palabra por estado final"<<endl; //Se acepta la palabra.
+        else cout<<"No acepta la palabra"<<endl;            //No se acepta la palabra.
       }
-      if(p != NULL){
-        cout<<"Acepta la palabra por estado final"<<endl;
-      }else if(stack.length()==0){
-        cout<<"Acepta la palabra por stack vacio"<<endl;
-      }else{
-        cout<<"No acepta la palabra"<<endl;
-      }
+      else if(stack.length()==0) cout<<"Acepta la palabra por stack vacio"<<endl; //Validacion de la palabra por stack vacio. Se acepta la palabra si el stack es de tamaño 0
+      else cout<<"No acepta la palabra"<<endl;                                    //No se acepta la palabra.
+
       cout<<"Para dejar de ingresar palabras, escriba finalizar \n";
     }
-
     exit(EXIT_SUCCESS);
 }
